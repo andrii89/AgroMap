4d4990011532656b66c9b11ec0a1057f1b2dd390
Andrii Bakutin <andrii@hummeling.com>
2018-12-11 18:05:19 +0200

    two activities are created: - main activity with a button to run the app - maps activity to process all features

---
+++ app/src/main/java/com/test/android/agromap/MapsActivity.java
@@ -0,0 +1,32 @@
+package com.test.android.agromap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapsActivity extends AppCompatActivity
+        implements
+        OnMapReadyCallback{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.activity_map);
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+
+    }
+}
